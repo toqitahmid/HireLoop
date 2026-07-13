75ed0e2fb4b4763cabd3f6fa625cf02277eddb5d
toqitahmid <rubaiathali@gmail.com>
2026-07-14 01:10:04 +0600

    create home page

---
+++ src/app/Home/page.jsx
@@ -0,0 +1,14 @@
+import HeroBanner from "../ui/Banner";
+import AppNavbar from "../ui/Navbar";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <HeroBanner></HeroBanner>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
