7fee52e60c41aa329e3d17b5eabd8c43aa59503d
toqitahmid <rubaiathali@gmail.com>
2026-08-12 20:44:15 +0600

    fix auth pages suspense and search params

---
+++ src/app/(homeLayout)/login/LogInForm.jsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+
+import { Zoom, toast } from "react-toastify";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { authClient } from "../../lib/auth-client";
+import { useState } from "react";
+
+const LogInForm = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get("redirect") || "/";
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const user = Object.fromEntries(formData.entries());
+
+    const { data, error } = await authClient.signIn.email({
+      email: user.email,
+      password: user.password,
+      rememberMe: true,
+    });
+
+    if (data) {
+      toast.success("You login successfully!", {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        theme: "dark",
+        transition: Zoom,
+      });
+
+      router.push(redirectTo);
+    } else if (error) {
+      console.error("Better-Auth Error Details:", error);
+
+      toast.error(
+        error.message || "Failed to login. Please check your inputs.",
+        {
+          position: "top-center",
+          theme: "dark",
+          transition: Zoom,
+        },
+      );
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-[60vh]">
+      <div className="flex items-center gap-3 my-10">
+        <h1 className="text-2xl font-semibold">Login</h1>
+      </div>
+
+      <Form
+        className="flex w-96 flex-col gap-4"
+        render={(props) => <form {...props} data-custom="foo" />}
+        onSubmit={onSubmit}
+      >
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        <TextField className="w-full" name="password">
+          <Label>Password</Label>
+
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full"
+              type={isVisible ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+
+        <div className="flex gap-2 justify-center">
+          <Button className="w-full" type="submit">
+            <Check />
+            Log in
+          </Button>
+
+          <Button className="w-full" type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+
+        <p className="text-center">
+          {`or haven't account`}{" "}
+          <span className="text-cyan-500 cursor-pointer">
+            <Link href={`/register?redirect=${redirectTo}`}>Register</Link>
+          </span>
+        </p>
+      </Form>
+    </div>
+  );
+};
+
+export default LogInForm;
